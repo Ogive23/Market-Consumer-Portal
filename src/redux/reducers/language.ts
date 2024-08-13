e7ef09f3760f0cf9df02be32733e89da9048b672
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2024-08-13 03:00:08 +0300

    Make Language Reducer Async

--- src/redux/reducers/language.ts
+++ src/redux/reducers/language.ts
@@ -1,16 +1,18 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { useTranslation } from "react-i18next";
 import { defaultLang, supportedLangs } from "../../config/i18nConfig";
 import en from "../../../public/static/translations/en.json";
 import ar from "../../../public/static/translations/ar.json";
 
 interface InitialState {
+  status: string;
   lang: string;
   supportedLangs: object;
   translations: object;
 }
 
 const initialState = {
+  status: "loading",
   lang: defaultLang,
   supportedLangs: { ...supportedLangs },
   translations: {
@@ -19,14 +21,33 @@ const initialState = {
   },
 } as InitialState;
 
+export const setLangAsync = createAsyncThunk(
+  "language/setLangAsync",
+  async (lang, { getState, dispatch }) => {
+    const resolvedLang =
+      window.localStorage.getItem("language") || getState().language.lang;
+    dispatch(language.actions.setLang(resolvedLang));
+    return resolvedLang;
+  }
+);
+
 export const language = createSlice({
   name: "language",
   initialState,
   reducers: {
     setLang: (state, action) => {
-      //   console.log("xD", action.payload);
       state.lang = action.payload;
+      window.localStorage.setItem("language", action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(setLangAsync.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(setLangAsync.fulfilled, (state, action) => {
+      state.lang = action.payload;
+      state.status = "idle";
+    });
   },
 });
 
