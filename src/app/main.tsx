6910195ec051e2c65ddbeed83abb4a9873a03510
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2024-08-13 03:00:44 +0300

    Make Lang Getting Async

---
+++ src/app/main.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { PaletteMode } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import AppAppBar from "@/components/ui/AppAppBar";
+import getLPTheme from "./getLPTheme";
+import LogoCollection from "@/components/ui/LogoCollection";
+import Footer from "@/components/ui/Footer";
+import FAQ from "@/components/ui/FAQ";
+import Pricing from "@/components/ui/Pricing";
+import Highlights from "@/components/ui/Highlights";
+import Testimonials from "@/components/ui/Testimonials";
+import Features from "@/components/ui/Features";
+import Hero from "@/components/ui/Hero";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectTranslations,
+  setLang,
+  setLangAsync,
+} from "@/redux/reducers/language";
+import { defaultLang } from "@/config/i18nConfig";
+
+export default function Main() {
+  const [mode, setMode] = React.useState<PaletteMode>("light");
+  const LPtheme = createTheme(getLPTheme(mode));
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
+  const i18nStatus: string = useSelector((state) => state.language.status);
+  const dispatch = useDispatch<any>();
+
+  React.useEffect(() => {
+    dispatch(setLangAsync());
+  }, []);
+  console.log(i18nStatus);
+  return i18nStatus === "loading" ? (
+    <h1>Loading...</h1>
+  ) : (
+    <ThemeProvider theme={LPtheme}>
+      {/* <div dir={locale == "ar" ? "rtl" : "ltr"}> */}
+      <CssBaseline />
+      <AppAppBar
+        mode={mode}
+        // language={locale}
+        toggleColorMode={toggleColorMode}
+        // toggleLanguageMode={toggleLanguageMode}
+      />
+      <Hero />
+      <Box sx={{ bgcolor: "background.default" }}>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </Box>
+      {/* </div> */}
+    </ThemeProvider>
+  );
+}
